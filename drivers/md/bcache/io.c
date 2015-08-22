61cb8559164c05150632f5728b19b8fc9bdb069d
Kent Overstreet <kent.overstreet@gmail.com>
2015-08-22 02:52:17 -0800

    bcache: bch_read() optimization

--- drivers/md/bcache/io.c
+++ drivers/md/bcache/io.c
@@ -1507,7 +1507,9 @@ void bch_read_extent(struct cache_set *c, struct bio *orig,
 	rbio->csum_type		= pick->crc.csum_type;
 	rbio->compression_type	= pick->crc.compression_type;
 
-	atomic_inc(&orig->bi_remaining);
+	if (!(flags & BCH_READ_IS_LAST))
+		atomic_inc(&orig->bi_remaining);
+
 	rbio->parent		= orig;
 	rbio->parent_iter	= orig->bi_iter;
 	rbio->c			= c;
@@ -1563,7 +1565,7 @@ void bch_read(struct cache_set *c, struct bio *bio, u64 inode)
 				      POS(inode, bio->bi_iter.bi_sector), k) {
 		struct extent_pick_ptr pick;
 		unsigned bytes, sectors;
-		bool done;
+		bool is_last;
 
 		EBUG_ON(bkey_cmp(bkey_start_pos(k.k),
 				 POS(inode, bio->bi_iter.bi_sector)) > 0);
@@ -1583,7 +1585,7 @@ void bch_read(struct cache_set *c, struct bio *bio, u64 inode)
 		sectors = min_t(u64, k.k->p.offset, bio_end_sector(bio)) -
 			bio->bi_iter.bi_sector;
 		bytes = sectors << 9;
-		done = bytes == bio->bi_iter.bi_size;
+		is_last = bytes == bio->bi_iter.bi_size;
 		swap(bio->bi_iter.bi_size, bytes);
 
 		if (pick.ca) {
@@ -1595,18 +1597,20 @@ void bch_read(struct cache_set *c, struct bio *bio, u64 inode)
 					bkey_start_offset(k.k),
 					BCH_READ_FORCE_BOUNCE|
 					BCH_READ_RETRY_IF_STALE|
-					BCH_READ_PROMOTE);
+					BCH_READ_PROMOTE|
+					(is_last ? BCH_READ_IS_LAST : 0));
 		} else {
 			zero_fill_bio(bio);
+
+			if (is_last)
+				bio_endio(bio, 0);
 		}
+
+		if (is_last)
+			return;
 
 		swap(bio->bi_iter.bi_size, bytes);
 		bio_advance(bio, bytes);
-
-		if (done) {
-			bio_endio(bio, 0);
-			return;
-		}
 	}
 
 	/*
